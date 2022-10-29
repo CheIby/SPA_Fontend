c2ea84efd6409282a67c5b8632dd97ebb7e6125f
CheIby <87913133+CheIby@users.noreply.github.com>
2022-10-30 05:38:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-const api_base = 'http://localhost:3001';
+const api_base = 'http://localhost:80/api/';
 
 function App() {
 	const [todos, setTodos] = useState([]);
